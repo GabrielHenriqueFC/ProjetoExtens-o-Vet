4b25fb3d2057859126df86b9465fad87c71dd1cb
Gabriel Henrique <131201351+GabrielHenriqueFC@users.noreply.github.com>
2024-05-24 10:56:54 -0300

    Site Veterinaria rework

---
+++ Scripts/login.js
@@ -0,0 +1,15 @@
+let container = document.getElementById('container')
+
+toggle = () => {
+	container.classList.toggle('sign-in')
+	container.classList.toggle('sign-up')
+}
+
+setTimeout(() => {
+	container.classList.add('sign-in')
+}, 200)
+
+
+function entrar() {
+    window.location.href = 'home.html'
+}
