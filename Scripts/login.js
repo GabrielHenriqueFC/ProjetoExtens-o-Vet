cc53b9f0eebb763eebcd0e69414f78af2552587d
GabrielHenriqueFC <131201351+GabrielHenriqueFC@users.noreply.github.com>
2024-05-27 15:53:56 -0300

    Implementação do Cadastro e Login

--- Scripts/login.js
+++ Scripts/login.js
@@ -1,5 +1,74 @@
-let container = document.getElementById('container')
+const usuario = document.querySelector('#user');
+const senha = document.querySelector('#senha');
+const email = document.querySelector('#email');
+const checkSenha = document.querySelector('#senha-confirm');
 
+let container = document.getElementById('container')
+var indice;
+
+
+//cadastro js
+function Enviar() {
+    var cadastro = {};
+    indice = localStorage.length;
+    cadastro.id = indice;
+    cadastro.nome = usuario.value;
+    cadastro.senha = senha.value;
+    cadastro.email = email.value;
+    cadastro.confirmaSenha = checkSenha.value;
+
+    // Função para verificar se o email já está cadastrado
+    function emailJaCadastrado(email) {
+        for (var i = 0; i < localStorage.length; i++) {
+            var item = JSON.parse(localStorage.getItem(localStorage.key(i)));
+            if (item.email === email) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    if (cadastro.nome !== "" && cadastro.email !== "" && cadastro.senha !== "" && cadastro.confirmaSenha !== "" 
+        && cadastro.senha === cadastro.confirmaSenha
+    ) {
+        if (emailJaCadastrado(cadastro.email)) {
+            alert('Este email já está cadastrado. Por favor, use outro email.');
+        } else {
+            localStorage.setItem(indice, JSON.stringify(cadastro));
+            alert('Obrigado sr(a) ' + cadastro.nome + ' cadastro realizado com sucesso');  
+        }
+    } else if (cadastro.senha !== cadastro.confirmaSenha) {
+        alert('Confirme corretamente sua senha.');
+    }
+}
+
+
+//Login
+function Login() {
+    var loginEmail = document.querySelector('#loginEmail').value;
+    var loginSenha = document.querySelector('#loginSenha').value;
+    var loginSuccess = false;
+
+    for (var i = 0; i < localStorage.length; i++) {
+        var item = JSON.parse(localStorage.getItem(localStorage.key(i)));
+        if (item.email === loginEmail && item.senha === loginSenha) {
+            alert('Login bem-sucedido! Bem-vindo, ' + item.nome + '!');
+            loginSuccess = true;
+			location.href = 'home.html';
+            break;
+        }
+    }
+
+    if (!loginSuccess) {
+        alert('Email ou senha incorretos.');
+    }
+}
+
+
+
+
+
+// animação js 
 toggle = () => {
 	container.classList.toggle('sign-in')
 	container.classList.toggle('sign-up')
